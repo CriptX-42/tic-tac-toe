aae71e9874cc31aeb641e719e8e0e757370f38da
Ricardo Carvalho <ricardo-svc@live.com>
2025-04-28 19:07:38 -0300

    🎉 - Iniciando layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,11 @@
 function App() {
 
   return (
-    <div className="text-3xl font-bold text-green-400">Olá mundo</div>
+    <main className="bg-blue-200 min-h-screen flex flex-col items-center justify-center">
+      <div className="w-full max-w-md bg-blue-100 p-8 rounded-2xl">
+        <h1 className="text-white font-bold text-center text-3xl">Tic Tac toe</h1>
+      </div>
+    </main>
   )
 }
 
