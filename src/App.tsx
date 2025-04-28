002f9ca684e0e14a4823e02dce9244c351991072
Ricardo Carvalho <ricardo-svc@live.com>
2025-04-28 19:20:43 -0300

    🎉  - Iniciando board

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
+import { Gamepad2 } from "lucide-react";
 
 function App() {
-
   return (
-    <main className="bg-blue-200 min-h-screen flex flex-col items-center justify-center">
-      <div className="w-full max-w-md bg-blue-100 p-8 rounded-2xl">
-        <h1 className="text-white font-bold text-center text-3xl">Tic Tac toe</h1>
+    <main className="flex min-h-screen flex-col items-center justify-center bg-blue-200">
+      <div className="w-full max-w-md rounded-2xl bg-blue-100 p-8">
+        <div className="mb-8 flex items-center justify-center gap-3">
+          <Gamepad2 className="h-8 w-8 text-pink" />
+          <h1 className="text-center text-3xl font-bold text-white">
+            Tic Tac toe
+          </h1>
+
+          <div className="mb-6 text-center">
+            <p className="text-xl font-semibold text-gray-100">
+              Player X' turn{" "}
+            </p>
+          </div>
+        </div>
       </div>
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
